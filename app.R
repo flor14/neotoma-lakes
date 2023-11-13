b4e66547bf07a271d32f8bc517caca2afaf6c5ec
Florencia D'Andrea <florencia.dandrea@gmail.com>
2023-11-12 16:34:16 -0800

    Add functional lake ploting for some countries

--- app.R
+++ app.R
@@ -6,35 +6,66 @@ library(stringr)
 library(dplyr)
 library(shinycssloaders)
 library(leafem)
+library(tidyr)
+library(ggplot2)
+# https://github.com/r-spatial/sf/issues/1762
+sf_use_s2(FALSE)
 
+# sites <- read.csv('../Dropbox/neotoma_project/sites_20231101_.csv')
+#
+# lakes <- st_read('../Dropbox/neotoma_project/HydroLAKES_polys_v10_shp/HydroLAKES_polys_v10_shp') |>
+#   select(Lake_name, Country, Continent, Vol_total, Shore_len, Depth_avg, Elevation, geometry)
+#
+# # I select some countries to try the app. Later I will use all.
+# can <- st_read('data/gadm41_CAN.gpkg') |>
+#   st_simplify(preserveTopology = TRUE, dTolerance = 100)
+#
+# arg <- st_read('data/gadm41_ARG.gpkg') |>
+#   st_simplify(preserveTopology = TRUE, dTolerance = 100)
+#
+# # jpn <- st_read('data/gadm41_JPN.gpkg') |>
+# #   st_simplify(preserveTopology = TRUE, dTolerance = 100)
+#
+# countries =  countries_sf = rbind(arg, can)
+#
+# st_geometry(countries) <- NULL
+#
+#
+# # Some sites are points and other ones are polygons
+# pointsites <- sites[stringr::str_detect(sites$geog, pattern = "POINT"), ] |>
+#   sf::st_as_sf(wkt = 'geog') |>
+#   select(siteid, sitename, geog) |>
+#   mutate(lonc = sf::st_coordinates(geog)[,1], # need this to setView in leaflet
+#          latc = sf::st_coordinates(geog)[,2])
+#
+# polysites <- sites[stringr::str_detect(sites$geog, pattern = "POLYGON"), ] |>
+#   sf::st_as_sf(wkt = 'geog') |>
+#   select(siteid, sitename, geog) |>
+#   st_simplify(preserveTopology = TRUE,
+#               dTolerance = 100) |>
+#   mutate(centroid = st_point_on_surface(geog),  # need this to setView in leaflet
+#          lonc = sf::st_coordinates(centroid)[,1],
+#          latc = sf::st_coordinates(centroid)[,2]) |>
+#   select(-centroid)
+#
+# datasf <- rbind(pointsites, polysites) |> st_set_crs(4326)
+#
+#
+# lakes_country <- countries |> left_join(lakes, by = c('COUNTRY' = 'Country'))
+# write_sf(lakes_country, "data/lakes_country.gpkg")
+lakes_country <- st_read("data/lakes_country.gpkg")
 
-sites <- read.csv('../Dropbox/neotoma_project/sites_20231101_.csv')
-#lakes <- st_read('https://www.dropbox.com/scl/fi/6g8m1p6wcnfytkyfriuvl/HydroLAKES_polys_v10.shp?rlkey=szmv5thra18ba5hkrd5284vt3&dl=1')
-
-# Some sites are points and other ones are polygons
-pointsites <- sites[stringr::str_detect(sites$geog, pattern = "POINT"), ] |>
-  sf::st_as_sf(wkt = 'geog') |> select(siteid, sitename, geog) |>
-  mutate(lonc = sf::st_coordinates(geog)[,1], # need this to setView in leaflet
-         latc = sf::st_coordinates(geog)[,2])
-
-polysites <- sites[stringr::str_detect(sites$geog, pattern = "POLYGON"), ] |>
-  sf::st_as_sf(wkt = 'geog') |> select(siteid, sitename, geog) |>
-  st_simplify(preserveTopology = TRUE, dTolerance = 100) |>
-  mutate(centroid = st_point_on_surface(geog),  # need this to setView in leaflet
-         lonc = sf::st_coordinates(centroid)[,1],
-         latc = sf::st_coordinates(centroid)[,2]) |>
-  select(-centroid)
-
-datasf <- rbind(pointsites, polysites)
-
+# sites_country <- datasf |> st_join(countries_sf, left = TRUE) |> tidyr::drop_na(COUNTRY)
+#write_sf(sites_country, "data/sites_country.gpkg")
+sites_country <- st_read("data/sites_country.gpkg")
 
 # Links
-link_neotoma <- tags$a(shiny::icon("github"), "Shiny", href = "https://github.com/rstudio/shiny", target = "_blank")
-link_slack <- tags$a(shiny::icon("r-project"), "Posit", href = "https://posit.co", target = "_blank")
+link_neotoma <- tags$a(shiny::icon("github"), "GitHub", href = "https://github.com/NeotomaDB", target = "_blank")
+link_slack <- tags$a(shiny::icon("slack"), "Slack", href = "https://neotomadb.slack.com/ssb/redirect", target = "_blank")
 
 
 ui <-  bslib::page_navbar(
-  title = "Neotoma Lakes",
+  title = "NeotomaDB",
   theme = bslib::bs_theme(bootswatch = "minty",
                    base_font = font_google("Inter")),
   bslib::nav_panel(title = "correct",
@@ -64,7 +95,7 @@ tags$h6('NeotomaDB data'),
  color.background = 'white')),
     card(
       card_header("Lake"),
-      'hello'
+      plotOutput('hydrolake')
     ),
     card(
 
@@ -96,15 +127,20 @@ server <- function(input, output, session) {
 
 
 
-  map_data <- eventReactive(input$search,ignoreInit = FALSE,{
+  neosites_data <- eventReactive(input$search,{
 
 
-
-
-    datasf |>
+    sites_country |>
        dplyr::mutate(siteid = as.character(siteid)) |>
       dplyr::filter(siteid %in% as.character(input$neositeid))
 
+  })
+
+
+  lake_data <- eventReactive(input$search,{
+
+
+    lakes_country[st_is_within_distance(neosites_data(), lakes_country, dist = 9000)[[1]],]
 
   })
 
@@ -123,18 +159,20 @@ server <- function(input, output, session) {
 
 
 
-   if(sf::st_is(map_data(), "POINT")){
+   if(sf::st_is(neosites_data(), "POINT")){
 
     lm |>
-       leaflet::addCircles(data = map_data()) |>
-       setView(lng = map_data()$lonc, lat= map_data()$latc, zoom = 9) |>
+       leaflet::addCircles(data = neosites_data()) |> # neotoma sites
+       leaflet::addPolygons(data = lake_data()) |> # lakes
+       setView(lng = neosites_data()$lonc, lat= neosites_data()$latc, zoom = 9) |>
        leafem::addMouseCoordinates()
 
    }else{
 
     lm |>
-       leaflet::addPolygons(data = map_data())  |>
-       setView(lng = map_data()$lonc, lat= map_data()$latc, zoom = 9) |>
+       leaflet::addPolygons(data = neosites_data())  |> # neotoma sites
+       leaflet::addPolygons(data = lake_data())  |> # lakes
+       setView(lng = neosites_data()$lonc, lat= neosites_data()$latc, zoom = 9) |>
        leafem::addMouseCoordinates()
 
    }
@@ -145,10 +183,20 @@ server <- function(input, output, session) {
 
   output$sitename <- renderText({
   #  req(input$search)
-   paste("sitename:", as.character(map_data()$sitename))
+   paste("sitename:", as.character(neosites_data()$sitename))
     })
 
 
+  output$hydrolake <- renderPlot({
+req(input$search)
+    ggplot()+
+      geom_sf(data = lake_data(),
+              fill = 'transparent',
+              lwd = 2)+
+      theme_void()
+
+
+  })
 
 
 }
