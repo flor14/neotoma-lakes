1df88433c33309c7d7441dd837962fe9e19805ce
Florencia D'Andrea <florencia.dandrea@gmail.com>
2023-12-07 16:06:38 -0800

    Add accordion for polygon created by the user and table with info

--- app.R
+++ app.R
@@ -87,22 +87,126 @@ sites_country <- st_read("data/sites_country.gpkg")
 
 
 # Links
-link_neotoma <- tags$a(shiny::icon("github"), "GitHub", href = "https://github.com/NeotomaDB", target = "_blank")
-link_slack <- tags$a(shiny::icon("slack"), "Slack", href = "https://neotomadb.slack.com/ssb/redirect", target = "_blank")
+link_neotoma <- tags$a(shiny::icon("github"),
+                       "GitHub",
+                       href = "https://github.com/NeotomaDB",
+                       target = "_blank")
+link_slack <- tags$a(shiny::icon("slack"),
+                     "Slack",
+                     href = "https://neotomadb.slack.com/ssb/redirect",
+                     target = "_blank")
 
+
+# Accordions
+
+accordion_left <-  accordion(
+  open = 'User options',
+  multiple = TRUE,
+  height="100%",
+  id = 'sidebar_accordion',
+  accordion_panel(
+    id = 'Site metadata',
+    title = 'Site metadata',
+    DT::DTOutput('metadata')),
+  accordion_panel(
+    id = 'Map options',
+    title = 'Map options',
+    shinyWidgets::materialSwitch(
+      inputId = "removelakes",
+      label = "remove HYDROlakes layer",
+      status = "primary"
+    )),
+  accordion_panel(
+    id = 'User options',
+    title = 'User options',
+    prettyRadioButtons( # or prettyRadioButtons
+      inputId = "modify",
+      label = "Is this site the correct lake polygon in NeotomaDB?",
+      choices = c("Yes", "No"),
+      outline = TRUE,
+      plain = TRUE,
+      status = "primary",
+      icon = icon("check")
+    ),
+    uiOutput('dynamicUI')
+  ))
+
+accordion_center <- accordion(
+  id = 'map_accordion',
+  open = 'How to use this app?',
+  height="100%",
+  accordion_panel(
+    id = "Map",
+    title = "Map displaying NeotomaDB and HYDROlakeDB",
+    class = 'p-0',
+    shinycssloaders::withSpinner(
+      leafletOutput('map',
+                    height = 620),
+      type = 2,
+      color = 'darkgreen',
+      color.background = 'white')),
+  accordion_panel(
+    id = "Comments",
+    title = "User decision",
+    layout_columns(
+      textOutput('action'),
+      tags$div(
+        textAreaInput('notes',
+                      label = 'Comments:'),
+        downloadButton('submit', 'Submit')))),
+  accordion_panel(
+    id = "help",
+    title = "How to use this app?",
+    tags$img(src = 'img-app.png',
+             height = 'auto',
+             width = '100%'),
+    includeMarkdown("help.md"),
+    tags$img(src = 'img-app-end.png',
+             height = 'auto',
+             width = '100%')))
+
+accordion_right <- accordion(
+  id = "lakes_or_polygons",
+  open = "Hydrolakes DB",
+  height="100%",
+  accordion_panel(
+    id = "lakes",
+    title = "Hydrolakes DB",
+
+      plotOutput('hydrolake',
+                 height = 190),
+      DT::DTOutput('lakeinfo'),
+      textOutput('dist',
+                 inline = TRUE),
+      textOutput('countryinfo'),
+      plotOutput('country',
+                 height = 190)
+
+  ),
+  accordion_panel(
+    id = "polygon",
+    title = "Polygon" ,
+    plotOutput('polygon_new'),
+    DT::DTOutput('polyinfo'),
+
+  ))
+
+
+# UI
 
 ui <-  bslib::page_navbar(
   title = "NeotomaDB",
   theme = bslib::bs_theme(version = 5,
                           bootswatch = "minty",
-                   base_font = font_google("Raleway", wght = "400"),
-                   heading_font = font_google("Raleway", wght = "200")),
-                 #  primary = 'orange'),
+                   base_font = font_google("Raleway",
+                                           wght = "400"),
+                   heading_font = font_google("Raleway",
+                                              wght = "200")),
   bslib::nav_panel(title = "Lakes",
                    shinyFeedback::useShinyFeedback(),
             bslib::layout_columns(
     col_widths = c(9, 3),
-    row_heights = c(1,1),
+    row_heights = list("auto", 1),
         bslib::layout_sidebar(class = "p-0",
                              sidebar = sidebar(width = 300,
                                               bg = 'white',
@@ -111,81 +215,11 @@ ui <-  bslib::page_navbar(
            placeholder = "write here"),
  tooltip(
  shiny::actionButton('search', 'Search!'),
- "Check sitesid in the webpage. For this demo you can use site 9606 as an example"),
- accordion(
-   open = 'User options',
-   multiple = TRUE,
-   height="100%",
-   id = 'sidebar_accordion',
-   accordion_panel(
-     id = 'Site metadata',
-     title = 'Site metadata',
-     DT::DTOutput('metadata')),
-   accordion_panel(
-     id = 'Map options',
-     title = 'Map options',
-   shinyWidgets::materialSwitch(
-     inputId = "removelakes",
-     label = "remove HYDROlakes layer",
-     status = "primary"
-   )),
- accordion_panel(
-   id = 'User options',
-   title = 'User options',
- prettyRadioButtons( # or prettyRadioButtons
-   inputId = "modify",
-   label = "Is this site the correct lake polygon in NeotomaDB?",
-   choices = c("Yes", "No"),
-   outline = TRUE,
-   plain = TRUE,
-   status = "primary",
-   icon = icon("check")
- ),
- uiOutput('dynamicUI')
- ))),
-accordion(
-  id = 'map_accordion',
-  open = 'How to use this app?',
-  height="100%",
-  accordion_panel(
-    id = "Map",
-    title = "Map displaying NeotomaDB and HYDROlakeDB",
-    class = 'p-0',
- shinycssloaders::withSpinner(
- leafletOutput('map', height = 600),
- type = 4,
- color = 'darkgreen',
- color.background = 'white')),
- accordion_panel(
-   id = "Comments",
-   title = "User decision",
-   layout_columns(
-     textOutput('action'),
-     tags$div(
-     textAreaInput('notes',
-              label = 'Comments:'),
-     downloadButton('submit', 'Submit')))),
-accordion_panel(
-  id = "help",
-  title = "How to use this app?",
-  tags$img(src = 'img-app.png',
-           height = 'auto',
-           width = '100%'),
-  includeMarkdown("help.md"),
-  tags$img(src = 'img-app-end.png',
-           height = 'auto',
-           width = '100%')))),
-    card(
-      card_header("HydroLAKES"),
-
-      plotOutput('hydrolake'),
-      DT::DTOutput('lakeinfo'),
-      textOutput('dist', inline = TRUE),
-      textOutput('countryinfo'),
-      plotOutput('country')
-
-    )
-  )),
+ paste("Check sitesid in the webpage.",
+       "For this demo you can use site 9606 as an example")),
+accordion_left),
+accordion_center),
+accordion_right)),
 nav_spacer(),
 nav_menu(
   title = "Links",
@@ -235,9 +269,12 @@ server <- function(input, output, session) {
   # Creation of a basic map
    lm =  leaflet::leaflet() |>
      leaflet::addTiles(group = "OpenStreetMap") |>
-      leaflet::addProviderTiles("Esri.WorldImagery",
-                                group = "Esri.WorldImagery",
-                                options = providerTileOptions(attribution = 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community - Powered by Esri')) |>
+     leaflet::addProviderTiles("Esri.WorldImagery",
+                      group = "Esri.WorldImagery",
+                      options = providerTileOptions(attribution = paste('Tiles',
+                      '&copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS,',
+                      'AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP,',
+                      'and the GIS User Community - Powered by Esri'))) |>
      leaflet::addLayersControl(
        baseGroups = c("EsriWorldImagery", "OpenStreetMap"),
        options = layersControlOptions(collapsed = FALSE)
@@ -337,9 +374,7 @@ server <- function(input, output, session) {
       proxy |>  hideGroup('lakes')
 
     }else{
-      proxy |>  showGroup('lakes')}
-
-      proxy <- leafletProxy('map')})
+      proxy |>  showGroup('lakes')}})
 
 
 
@@ -363,7 +398,26 @@ observe({
   # Open the accordion to complete and submit comments once they have selected
   # what they want to do with the siteid lake data
   accordion_panel_open(id = 'map_accordion',
-                       value = 'User decision')}
+                        value = 'User decision')} })
+
+observe({
+
+  req(input$nooptions)
+  if(input$modify == "No" && input$nooptions == "Create lake polygon"){
+
+    # Open the accordion when the user wants to create a polygon
+    accordion_panel_close(id = 'lakes_or_polygons',
+                         value = 'Hydrolakes DB')
+    accordion_panel_open(id = 'lakes_or_polygons',
+                         value = 'Polygon')
+
+  }else if(input$modify == "No" &&
+          input$nooptions == "Replace with HYDROLakeDB"){
+
+  accordion_panel_close(id = 'lakes_or_polygons',
+                        value = 'Polygon')
+  accordion_panel_open(id = 'lakes_or_polygons',
+                       value = 'Hydrolakes DB')}
 })
 
 
@@ -393,7 +447,7 @@ observe({
       prettyRadioButtons( # or prettyRadioButtons
         inputId = "nooptions",
         label = "How would you update NeotomaDB?",
-        choices = c("Replace with HYDROLake polygon" ,
+        choices = c("Replace with HYDROLakeDB" ,
                     "Create lake polygon"),
         outline = TRUE,
         plain = TRUE,
@@ -546,7 +600,8 @@ observe({
     req(input$map_shape_click$id)
 
       if(!is.null(input$map_shape_click$id)){
-      csf =  countries_sf |> dplyr::filter(COUNTRY == lk_click()$COUNTRY)
+      csf =  countries_sf |>
+        dplyr::filter(COUNTRY == lk_click()$COUNTRY)
 
       paste("Country:", as.character(lk_click()$COUNTRY))
 
@@ -566,12 +621,13 @@ observe({
 
       paste("The NeotomaDB site", input$neositeid, "is correct and don't need to be replaced")
 
-    }else if(input$nooptions == "Replace with HYDROLake polygon"){
+    }else if(input$nooptions == "Replace with HYDROLakeDB"){
 
 
       # Message
       validate(
-        need(!is.null(input$map_shape_click), "Please, click one of the HYDROlakes in the map")
+        need(!is.null(input$map_shape_click),
+             "Please, click one of the HYDROlakes in the map")
       )
 
       paste("The NeotomaDB site", input$neositeid,
@@ -582,7 +638,8 @@ observe({
 
       # Message
       validate(
-        need(!is.null(input$map_draw_new_feature), "Please, create a polygon in the map")
+        need(!is.null(input$map_draw_new_feature),
+             "Please, create a polygon in the map")
       )
 
 
@@ -592,9 +649,14 @@ observe({
 
   })
 
-  observe({
 
+  polygon_sf <- reactive({
 
+    coords = input$map_draw_new_feature$geometry$coordinates
+    coords_matrix <- sapply(coords[[1]], unlist)
+    polygon <- st_polygon(list(t(coords_matrix)))
+    polygon_sf <- st_sfc(polygon, crs = 4326) # meters
+    polygon_sf |> st_transform(3857)
   })
 
 
@@ -608,35 +670,28 @@ observe({
      data_submit <-  data.frame('siteId' = input$neositeid,
                                  'comments' = input$notes)
 
-    }else if(input$modify == "No" & input$nooptions == "Replace with HYDROLake polygon"){
+    }else if(input$modify == "No" &&
+             input$nooptions == "Replace with HYDROLakeDB"){
 
       data_submit <-  data.frame('siteId' = input$neositeid,
                                  'HYDROlake_id' = lk_click()$Hylak_id,
                                  'comments' = input$notes)
 
-    }else if(input$modify == "No" & input$nooptions == "Create lake polygon"){
+    }else if(input$modify == "No" &&
+             input$nooptions == "Create lake polygon"){
 
 
 
       if(!is.null(input$map_draw_new_feature)){
-        # list of lists
-        coords = input$map_draw_new_feature$geometry$coordinates
-        print(coords)
-        coords_matrix <- sapply(coords[[1]], unlist)
-        polygon <- st_polygon(list(t(coords_matrix)))
-        polygon_sf <- st_sfc(polygon, crs = 4326)
-        print(polygon_sf)
 
-
+         polygon_sf()
       }
 
 
       data_submit <-  data.frame('siteId' = input$neositeid,
                                  'comments' = input$notes)
 
-      print(data_submit)
-      poly_submit = st_sf(data_submit, geometry = polygon_sf)
-      print(poly_submit)
+      poly_submit = st_sf(data_submit, geometry = polygon_sf())
       poly_submit
      }
 
@@ -659,7 +714,8 @@ observe({
         print(data_submit())
         write.csv(data_submit(), fname)
 
-      }else if(input$modify == 'No' & input$nooptions == 'Create lake polygon'){
+      }else if(input$modify == 'No' &&
+               input$nooptions == 'Create lake polygon'){
 
         print(data_submit())
         st_write(data_submit(), fname, layer_options = "GEOMETRY=AS_WKT")
@@ -674,6 +730,61 @@ observe({
     }
   )
 
+
+  # Table with HYDROlakeDB lake clicked data
+  output$polyinfo <- DT::renderDT({
+
+    if(!is.null(input$map_draw_new_feature)){
+
+      bbox <- st_bbox(polygon_sf())
+      units <- st_crs(polygon_sf(),
+                      parameters = TRUE)$units_gdal
+      print(st_crs(polygon_sf(),
+                   parameters = TRUE))
+      datap <- t(data.frame(c('Min Latitude',
+                              bbox$xmin),
+                            c('Max Latitude',
+                              bbox$xmax),
+                            c('Min Longitude',
+                              bbox$ymin),
+                            c('Max Longitude',
+                              bbox$ymax),
+                            c(paste0('Area (', units, ')'),
+                               round(st_area(polygon_sf()),
+                                     digits = 2)),
+                             c(paste0('Shore length (', units, ')'),
+                               round(st_length(st_boundary(polygon_sf())),
+                                     digits = 2))))
+
+      colnames(datap) <- NULL
+
+      DT::datatable(datap,
+                    rownames= FALSE,
+                    options = list(ordering = FALSE,
+                                   dom = 't'), # remove table interactive default options
+                    colnames = rep("", ncol(datap))) # remove column names
+
+    }
+
+  })
+
+
+  output$polygon_new <- renderPlot({
+
+    # Message
+    validate(
+      need(!is.null(input$map_draw_new_feature),
+           "Please, create a polygon in the map")
+    )
+
+    # plot recently created polygon
+      ggplot()+
+        geom_sf(data = polygon_sf(),
+                fill = 'aliceblue',
+                lwd = 0.5)+
+        theme_void()
+
+  })
 
 
 }
